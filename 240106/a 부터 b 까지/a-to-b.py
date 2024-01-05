c45e5066841082a26d2f3694f636df093959dc2b
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-06 01:28:00 +0900

    Title: a 부터 b 까지, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240106/a 부터 b 까지/a-to-b.py
+++ 240106/a 부터 b 까지/a-to-b.py
@@ -3,9 +3,8 @@ arr=input().split()
 a,b=int(arr[0]),int(arr[1])
 
 while a<=b:
+    print(a,end=' ')
     if a%2==0:
-        print(a,end=' ')
         a+=3
     else:
-        print(a,end=' ')
         a*=2
