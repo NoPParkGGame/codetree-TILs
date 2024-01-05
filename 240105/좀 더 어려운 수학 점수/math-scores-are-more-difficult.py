096016ffebfc77d7a66c8eb0935583ab30a31966
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-05 22:24:50 +0900

    Title: 좀 더 어려운 수학 점수, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240105/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240105/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,7 +1,7 @@
 A=input().split() #A=[95, 90]
 B=input().split() #B=[95, 80]
 A_math, A_eng= int(A[0]),int(A[1]) #A_math= 95, A_eng=90
-B_math, B_eng=int(B[0]), int(A[1]) #B_math= 95, B_eng= 80
+B_math, B_eng=int(B[0]), int(B[1]) #B_math= 95, B_eng= 80
 
 if A_math==B_math: #True
     if A_eng>B_eng: #90>80
