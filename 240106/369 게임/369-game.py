638b48d0c4d6c317b6df5b2e2be9bdcedaec75b4
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-06 02:05:01 +0900

    Title: 369 게임, Time: 172ms, Memory: 24MB, Status: Passed - CodeTree

--- 240106/369 게임/369-game.py
+++ 240106/369 게임/369-game.py
@@ -7,15 +7,15 @@ for i in range(1,n+1):
         if 9<i<100: #ex) 86
             a=i//10 #10의자리 숫자 8
             b=i%10 # 1의자리 숫자 6
+            indi=0
             for j in indicator:
-                indi=0
                 if a==j or b==j:
                     indi=1
                 else:
                     pass
             if indi==1:
                 print(0,end=' ')
-
+                
             else:
                 print(i,end=' ')
 
