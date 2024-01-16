3916053aa76a5d2b4994fbea99be53699239913f
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-16 23:06:23 +0900

    Title: 직각 삼각형 출력, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240116/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,6 +1,14 @@
 n=int(input())  #n=5
 
 for i in range(n): # i는 0 , 1 , 2 , 3 , 4
-    for j in range(i+1,n+1,2):    #j는 1, 3, 5
-        print('*',end='')
+    for j in range((i*2)+1):
+        print('*',end='')        
     print()
+
+'''
+    i=0 j=1
+    i=1 j=3
+    i=2 j=5
+
+    j=i*2+1
+'''
