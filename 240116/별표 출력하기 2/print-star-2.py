0295a30429f8b38c028ce0feff40ac0df1a85d9d
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-16 22:31:40 +0900

    Title: 별표 출력하기 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/별표 출력하기 2/print-star-2.py
+++ 240116/별표 출력하기 2/print-star-2.py
@@ -1,6 +1,12 @@
 n=int(input())
-
+'''
 for i in range(n+1,0,-1):
     for _ in range(i-1):
         print('*',end=' ')
     print('')
+'''
+
+for i in range(n):
+    for j in range(n-i):
+        print('*',end=' ')
+    print()
