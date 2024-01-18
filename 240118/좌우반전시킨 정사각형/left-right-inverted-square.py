4613428919aa63e15dbcd50783a3850414fa230b
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-18 23:28:07 +0900

    Title: 좌우반전시킨 정사각형, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/좌우반전시킨 정사각형/left-right-inverted-square.py
+++ 240118/좌우반전시킨 정사각형/left-right-inverted-square.py
@@ -1,18 +1,16 @@
 n=int(input())
-cnt=1
-for i in range(1,n+1):
-    cnt=0
-    cnt+=i
-    for j in range(1,n+1):
-        if j==1:
-            print(4*i,end=' ')
-        else:
-            print(4*i-cnt,end=' ')
-            cnt+=i
 
+for i in range(1,n+1):
+    for j in range(n,0,-1):
+        print(i*j,end=" ")
     print()
+
+
+
+
 '''
-   1 2 3 4
+n=4
+   4 3 2 1
 
 1 /4 3 2 1
 2 /8 6 4 2
