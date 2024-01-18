cfc1b2433abf1eedc064cd292b2d288a96980600
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-18 23:54:09 +0900

    Title: 좌우 지그재그 출력, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240118/좌우 지그재그 출력/print-lr-zigzag.py
+++ 240118/좌우 지그재그 출력/print-lr-zigzag.py
@@ -1,6 +1,9 @@
 n=int(input())
 cnt=1
-max_val=n*n-(n-1)
+if n%2==1:
+    max_val=n*n
+else:
+    max_val=n*n-n+1
 for i in range(n):
     if i%2==0:
         for j in range(n):
@@ -9,6 +12,19 @@ for i in range(n):
         print()
     else:
         for j in range(n):
-            print(n*n-cnt+1,end=' ')
+            print(max_val-cnt+1,end=' ')
             cnt+=1
         print()
+        '''
+    4 5 6
+    6 5 4
+
+    cnt+cnt+n-1
+    5 6 7 8 
+    8 7 6 5
+    i=1 j=cnt
+
+    16 15 14 13
+    13 14 15 16
+    i=3 j= cnt
+    '''
