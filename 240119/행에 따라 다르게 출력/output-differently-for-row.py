82f9901f39f9a929f90c5a24e8f55b9b17bbd18a
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-19 23:08:39 +0900

    Title: 행에 따라 다르게 출력, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240119/행에 따라 다르게 출력/output-differently-for-row.py
@@ -0,0 +1,16 @@
+n=int(input())
+cnt=1
+for i in range(n):
+    if i%2==0:
+        for j in range(n):
+            print(cnt,end=' ')
+            if j<n-1:
+                cnt+=1
+        cnt+=2
+    else:
+        for j in range(n):
+            print(cnt,end=' ')
+            if j<n-1:
+                cnt+=2
+        cnt+=1        
+    print()
