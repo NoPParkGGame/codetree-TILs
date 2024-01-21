b46bf038b4916888ed0dd7c59843f56953488b20
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-21 23:35:28 +0900

    Title: 학점 계산기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240121/학점 계산기/credit-calculator.py
@@ -0,0 +1,12 @@
+sub_n=int(input())
+
+arr=list(map(float,input().split()))
+
+avg=sum(arr)/sub_n
+print(f'{avg:.1f}')
+if avg>=4.0:
+    print('Perfect')
+elif avg>=3.0:
+    print('Good')
+else:
+    print('Poor')
