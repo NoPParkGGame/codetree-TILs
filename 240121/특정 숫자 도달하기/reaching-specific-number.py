05be4246d600a5fa6b649d3e4ec5287a71a0c77e
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-21 23:25:26 +0900

    Title: 특정 숫자 도달하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240121/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,10 @@
+arr=list(map(int,input().split()))
+sum_val=0
+cnt_val=0
+for elem in arr:
+    if elem<250:
+        sum_val+=elem
+        cnt_val+=1
+    else:
+        break
+print(sum_val,sum_val/cnt_val)
