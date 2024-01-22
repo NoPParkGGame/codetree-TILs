b69b78d51408d9dc3fbe4a7138a923aa4cb4b35a
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-22 22:56:06 +0900

    Title: 숫자들의 배수, Time: 112ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240122/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,14 @@
+n=int(input())
+cnt=1
+mult_cnt=0
+arr=[]
+while True:
+    arr.append(n*cnt)
+    cnt+=1
+    if n*cnt%5==0:
+        mult_cnt+=1
+    if mult_cnt>=2:
+        arr.append(n*cnt)
+        break
+for elem in arr:
+    print(elem,end=' ')
