67fded7cc6193d6e3e0ac7a37fbe2341143d88b4
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-22 22:46:05 +0900

    Title: 일의 자리 배열, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/일의 자리 배열/array-with-units.py
+++ 240122/일의 자리 배열/array-with-units.py
@@ -1,5 +1,5 @@
 arr=list(map(int,input().split()))
-
+'''
 for i in range(3,11):
     if arr[-1]+arr[-2]<10:
         arr.append(arr[-1]+arr[-2])
@@ -8,3 +8,10 @@ for i in range(3,11):
 
 for elem in arr:
     print(elem,end=' ')
+
+'''
+for i in range(3,11):
+    arr.append((arr[-1]+arr[-2])%10)
+
+for elem in arr:
+    print(elem, end=' ')
