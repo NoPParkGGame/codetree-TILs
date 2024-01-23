7bb71bfc378988866052a8fc1dc688b245569416
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-24 00:44:15 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240124/배열 놀이/play-with-array.py
+++ 240124/배열 놀이/play-with-array.py
@@ -5,13 +5,16 @@ n_arr=list(map(int,input().split()))
 for _ in range(q):
     q_arr=list(map(int,input().split()))
     if len(q_arr)==2:
+        a = q_arr[1]
         if q_arr[0]==1:
-            print(n_arr[n_arr.index(q_arr[1])])
+            print(n_arr[n_arr.index(a)])
         else:
-            if q_arr[1] in n_arr:
-                print(n_arr.index(q_arr[1])+1)
+            if a in n_arr:
+                print(n_arr.index(a)+1)
             else:
                 print(0)
     else:
-        for elem in n_arr[q_arr[1]-1:q_arr[2]]:
+        a = q_arr[1]
+        b = q_arr[2]
+        for elem in n_arr[a-1:b]:
             print(elem, end=' ')
