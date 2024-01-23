ed414411ce3158daf518949afb5d1b3364ec70de
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-23 22:15:45 +0900

    Title: 십의 자리 숫자, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240123/십의 자리 숫자/number-of-tens-digit.py
+++ 240123/십의 자리 숫자/number-of-tens-digit.py
@@ -1,13 +1,12 @@
 arr=list(map(int,input().split()))
-new_arr=[elem//10 for elem in arr]
 
-cnt_arr=[]
-for i in range(1,10):
-    cnt_arr.append(i)
-res_arr=[]
-for i in range(len(cnt_arr)):
+teen_arr=[elem//10 for elem in arr]
+
+count_arr=[i for i in range(1,10)]
+
+for counter in count_arr:
     cnt=0
-    for j in range(len(new_arr)):
-        if cnt_arr[i]==new_arr[j]:
+    for subj in teen_arr:
+        if counter==subj:
             cnt+=1
-    print(f'{cnt_arr[i]} - {cnt}')
+    print(counter,'-',cnt)
