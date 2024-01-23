d722b665711cc1228e81f6ebab2d55289f37297b
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-24 00:48:15 +0900

    Title: 배열 놀이, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240124/배열 놀이/play-with-array.py
+++ 240124/배열 놀이/play-with-array.py
@@ -7,7 +7,7 @@ for _ in range(q):
     if len(q_arr)==2:
         a = q_arr[1]
         if q_arr[0]==1:
-            print(n_arr[n_arr.index(a)])
+            print(n_arr[a-1])
         else:
             if a in n_arr:
                 print(n_arr.index(a)+1)
