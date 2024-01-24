aa6a4792154815a814585ae3d6eff8b2f7cd968d
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-24 23:24:25 +0900

    Title: n개의 숫자 중 최대 2개, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240124/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -6,19 +6,18 @@ arr=list(map(int,input().split()))
 
 max_val=-sys.maxsize
 
+sec_arr=[]
+
 for elem in arr:
     if max_val<elem:
         max_val=elem
 sec_val=max_val
 
 if arr.count(max_val)<2:
-
-    for i in range(max_val):
-        sec_val-=1
-        if sec_val in arr:
-            break
-else:
-    pass
-
+    arr.remove(max_val)
+    sec_val=-sys.maxsize
+    for elem in arr:
+        if sec_val<elem:
+            sec_val=elem
 
 print(max_val, sec_val)
