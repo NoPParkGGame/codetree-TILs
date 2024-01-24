4e756de85d7b004bff962bbb3535f54056f5256f
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-24 22:43:40 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240124/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -4,11 +4,9 @@ arr=list(map(int,input().split()))
 
 obj=2
 cnt_2=0
-cnt_arr=1
 for i in range(n):
     if arr[i]==obj:
         cnt_2+=1
         if cnt_2==3:
-            print(cnt_arr)
+            print(i+1)
             break
-    cnt_arr+=1
