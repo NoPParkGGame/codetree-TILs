353ebe9c83b5c007d1731fb9d3a8d7807fe7fd08
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-27 00:25:08 +0900

    Title: 아래로 사각형 채우기, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240127/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ 240127/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -5,21 +5,14 @@ arr=[
     [0 for _ in range(n)]
     for _ in range(n)
 ]
-
 num=1
+
 for i in range(n):
-    n1=num
-    n2=num
-    if i%2==0:
-        for j in range(n): 
-            arr[i][j]=n1
-            n1+=n
-    else:
-        for j in range(n):
-            arr[i][j]=n2
-            n2+=n
-    num+=1
+    for j in range(n):
+        arr[j][i]=num
+        num+=1
+
 for row in arr:
     for elem in row:
-        print(elem,end=' ')
+        print(elem, end=' ')
     print()
