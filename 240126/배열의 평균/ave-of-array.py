5ab2f1fb5395b3f1085535eb937dd10259439fa3
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-26 22:53:41 +0900

    Title: 배열의 평균, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240126/배열의 평균/ave-of-array.py
@@ -0,0 +1,21 @@
+arr_2r_4c=[
+    list(map(int,input().split()))
+    for _ in range(2)
+]
+total_sum_val=0
+total_count=0
+for elem in arr_2r_4c:
+    print(sum(elem)/len(elem),end=' ')
+    total_sum_val+=sum(elem)/len(elem)
+    total_count+=1
+print()
+
+for j in range(4):
+    sum_val=arr_2r_4c[0][j]+arr_2r_4c[1][j]
+    print(sum_val/2,end=' ')
+    total_sum_val+=sum_val/2
+    total_count+=1
+    
+print()
+
+print(total_sum_val/total_count)
