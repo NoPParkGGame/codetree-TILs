652b32b7ddcf7d69b56125588d50b4f815f89ac1
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-26 22:36:35 +0900

    Title: 두 숫자의 차의 최솟값, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 240126/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -6,17 +6,27 @@ arr=list(map(int,input().split()))
 min_val=sys.maxsize
 
 for i in range(len(arr)):
-    for j in range(1,len(arr)-(i+1)):
-        if arr[i+j]-arr[i]<min_val:
-            min_val=arr[i+j]-arr[i]
+    for j in range(i+1,len(arr)):
+        if arr[j]-arr[i]<min_val:
+            min_val=arr[j]-arr[i]
 
 print(min_val)
 
+
+
 '''
-0   1234
-1   123
-2   12
+len(arr)==5
+i   j
+0   1  
+0   2
+0   3
+0   4
 
+1   2
+1   3
+1   4
 
+2   3  
+3   4
 
 '''
