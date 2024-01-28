a4519f750ff200944d7f8d5ac7cda7f447899725
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-28 23:16:43 +0900

    Title: 대각선으로 숫자 채우기, Time: 5057ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 240128/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,39 @@
+n,m =tuple(map(int,input().split()))
+
+arr=[
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+cnt=0
+num=1
+
+while cnt<=m*n:
+    for i in range(cnt+1):
+        for j in range(cnt+1):
+            if i+j==cnt and i<n and j<m:
+                arr[i][j]=num
+                num+=1
+    cnt+=1
+
+for row in arr:
+    for elem in row:
+        print(elem,end=' ')
+    print()
+
+
+'''
+0 0
+
+0 1
+1 0
+
+0 2
+1 1
+2 0
+
+1 2
+2 1
+
+2 2
+'''
