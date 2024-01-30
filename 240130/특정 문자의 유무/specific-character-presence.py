02c3b5b4d834b93e331ae476567337be396e7045
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-30 23:01:06 +0900

    Title: 특정 문자의 유무, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/특정 문자의 유무/specific-character-presence.py
+++ 240130/특정 문자의 유무/specific-character-presence.py
@@ -9,10 +9,10 @@ for i in range(len(s)-1):
     if s[i:i+2]=='ab':
         exisit_ab=True
 if exisit_ee==True:
-    print('Yes')
-else:
-    print('No')
-if exisit_ab==True:
     print('Yes',end=' ')
+else:
+    print('No',end=' ')
+if exisit_ab==True:
+    print('Yes')
 else:
     print('No')
