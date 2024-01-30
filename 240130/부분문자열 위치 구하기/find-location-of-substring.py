1faf98309229c70c51d4b4701ef58f6906ea4ab2
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-01-30 23:32:33 +0900

    Title: 부분문자열 위치 구하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240130/부분문자열 위치 구하기/find-location-of-substring.py
+++ 240130/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,4 +1,11 @@
 string=input()
 alph=input()
 
-print(string.find(alph))
+exists=False
+
+for i in range(len(string)-len(alph)+1):
+    if string[i:i+len(alph)]==alph:
+        exists=True
+        print(i)
+if exists==False:
+    print(-1)
