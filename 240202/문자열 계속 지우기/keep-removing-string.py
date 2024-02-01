36e5ceec11a929bf36aeaf3d16c6799cfc26222b
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-02 01:11:49 +0900

    Title: 문자열 계속 지우기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240202/문자열 계속 지우기/keep-removing-string.py
+++ 240202/문자열 계속 지우기/keep-removing-string.py
@@ -2,7 +2,7 @@ string_A = list(input())
 string_B= list(input())
 
 while ''.join(string_B) in ''.join(string_A):
-    for i in range(len(string_A)-len(string_B)):  #0 부터 4까지 돌거임 5 이상 index out
+    for i in range(len(string_A)-len(string_B)+1):  #0 부터 4까지 돌거임 5 이상 index out
         if string_A[i:i+len(string_B)]==string_B:
             string_A=''.join(string_A)
             string_A= string_A[:i]+string_A[i+len(string_B):]
