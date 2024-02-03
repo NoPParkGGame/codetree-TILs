5b21c880ea63172b5cd06ac955baa1b0576918e4
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-03 22:33:25 +0900

    Title: 대문자 소문자 바꾸기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
+++ 240203/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
@@ -3,5 +3,5 @@ string=input()
 for elem in string:
     if ord('A')<= ord(elem) <ord('Z'):
         print(elem.lower(), end='')
-    if ord('a')<= ord(elem) <ord('z'):
+    else:
         print(elem.upper(), end='')
