556091e53a838fc0ec7d6bd0c67c8c3f9ecc4f1a
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-03 22:56:35 +0900

    Title: 미는 횟수, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/미는 횟수/number-of-pushes.py
+++ 240203/미는 횟수/number-of-pushes.py
@@ -2,17 +2,12 @@ string_A= input()
 
 string_B= input()
 cnt=0
-indicator=True
 while True:
     string_A=string_A[-1]+string_A[:-1]
     cnt+=1
     if string_A==string_B:
+        print(cnt)
         break
     if cnt>len(string_A):
-        indicator=False
+        print(-1)
         break
-
-if indicator==False:
-    print(-1)
-else:
-    print(cnt)
