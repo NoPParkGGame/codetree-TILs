698898f09f5d12f6e976ad601c8bca732694e746
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-04 23:55:35 +0900

    Title: 함수를 이용한 윤년 판별, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240204/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240204/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -3,7 +3,7 @@ def is_feb_29(y):
         return False
     if y%4==0 and y%100==0:
         return False
-    if y%4!=0 and y%100!=0 and y%400!=0:
+    if (y%4!=0 and y%100!=0) or (y%400!=0):
         return False
     return True
 
