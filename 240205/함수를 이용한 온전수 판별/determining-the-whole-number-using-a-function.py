fd4369dc63c529625e1e7e415fd92093ca272237
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-05 00:44:46 +0900

    Title: 함수를 이용한 온전수 판별, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240205/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,14 @@
+def whether_num(n):
+    if n%2==0:
+        return False
+    if n%10==5:
+        return False
+    if n%3==0 and n%9!=0:
+        return False
+    return True
+a, b= tuple(map(int, input().split()))
+cnt=0
+for i in range(a, b+1):
+    if whether_num(i):
+        cnt+=1
+print(cnt)
