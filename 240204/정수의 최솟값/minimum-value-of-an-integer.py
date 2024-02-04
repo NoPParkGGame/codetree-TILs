546808f563c5e3c9dc63c7012fb6a5ee36ed63c5
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-04 22:46:01 +0900

    Title: 정수의 최솟값, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240204/정수의 최솟값/minimum-value-of-an-integer.py
@@ -0,0 +1,12 @@
+def get_min(a, b, c):
+    import sys
+    min_val=sys.maxsize
+    arr=[a, b, c]
+    for elem in arr:
+        if min_val>elem:
+            min_val=elem
+    return min_val
+
+a, b, c= list(map(int, input().split()))
+
+print(get_min(a, b, c))
