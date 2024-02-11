b5f8f11ed4df2ffa0bf53104ffee7387bba457d5
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-12 00:57:11 +0900

    Title: 큰 숫자 자리수의 합, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
+++ 240212/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
@@ -6,14 +6,14 @@ def f(n):
     
     return f(n-1) * arr[n]
 
-def g(n):
+def g(n=f(len(arr)-1)):
     if n<10:
         return n
     return g(n//10) + n%10
 
 
 
-print(g(f(len(arr)-1)))
+print(g())
 
 
 '''
