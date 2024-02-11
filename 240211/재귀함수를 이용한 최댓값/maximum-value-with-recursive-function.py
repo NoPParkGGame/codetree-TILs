14ffe1258beb955ed017a292e1c2bef496ac8dc4
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-11 23:52:09 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240211/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240211/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -7,9 +7,11 @@ arr.append(-sys.maxsize)
 def f(n):
     if n==1:
         return arr[0] if arr[0]>arr[1] else arr[1]
-        
-    if f(n-1) > arr[n]:
-        return arr[n-1]
+    
+    cached=int(f(n-1))
+
+    if cached > arr[n]:
+        return cached
 
     return arr[n]
 
