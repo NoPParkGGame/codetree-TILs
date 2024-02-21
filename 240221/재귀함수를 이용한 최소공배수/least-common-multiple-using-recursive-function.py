da2095b9a04a19cba10c4180299abd11878afe58
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-21 22:56:47 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240221/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240221/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -9,7 +9,7 @@ def max_(a, b): #최대공약수 구하기
     return max_(b, a%b) # 값이 0이 아니라면 a, b의 최대공약수와 b, a%b의 최대공약수가 같음을 이용해 재귀함수 호출
 
 def f(n): #최소 공배수 = 두 수의 곱 / 최대공약수, 즉 두 수씩 나누어서 재귀함수 호출
-    if n==1: # n이 3이면 
+    if n==1: # n이 1이면 
         return (arr[0]*arr[1]) // max_(arr[0],arr[1]) # 0번째 값과 1번째 값의 최소공배수를 return
     minus=f(n-1)
     return minus * arr[n] // max_(minus, arr[n])   
