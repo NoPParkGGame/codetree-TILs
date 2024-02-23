da8b9580d7c9571bc12d6d9368c101d39b552926
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-23 22:20:10 +0900

    Title: 중앙값 계산 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240223/중앙값 계산 2/get-median-2.py
+++ 240223/중앙값 계산 2/get-median-2.py
@@ -1,18 +1,10 @@
 n=int(input())
 
 arr=list(map(int, input().split()))
-sorted_arr=sorted(arr)
+new_arr=[]
 
 for i in range(len(arr)):
-    if n<=2:
-        print(arr[0])
-        break
-    elif i%2==0:
-        print(sorted_arr[(i+1)//2], end=' ')
-
-
-
-
-'''
-y=(x+1)//2
-'''
+    new_arr.append(arr[i])
+    if i%2==0:
+        sort.new_arr()
+        print(new_arr[(i+1)//2])
