fcc7b57cc15d2a1d26ec1180b6afca18fbc105b9
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-23 22:20:44 +0900

    Title: 중앙값 계산 2, Time: 194ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/중앙값 계산 2/get-median-2.py
+++ 240223/중앙값 계산 2/get-median-2.py
@@ -6,5 +6,5 @@ new_arr=[]
 for i in range(len(arr)):
     new_arr.append(arr[i])
     if i%2==0:
-        sort.new_arr()
-        print(new_arr[(i+1)//2])
+        new_arr.sort()
+        print(new_arr[(i+1)//2], end=' ')
