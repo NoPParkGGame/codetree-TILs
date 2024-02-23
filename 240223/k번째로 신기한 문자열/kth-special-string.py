503de6b799d2c70dceb4accdb730f24f1eaed215
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-23 12:34:20 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/k번째로 신기한 문자열/kth-special-string.py
@@ -0,0 +1,17 @@
+n, k ,T = input().split()
+n, k = int(n), int(k)
+
+arr=[]
+
+for _ in range(n):
+    string=input()
+    indicator=True
+    for i in range(len(T)):
+        if T[i] !=string[i]:
+            indicator=False
+    if indicator==True:
+        arr.append(string)
+
+arr.sort()
+
+print(arr[k-1])
