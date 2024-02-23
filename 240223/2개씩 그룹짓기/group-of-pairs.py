323e4cd9e30f28b6a4c9b59e637363ec1299dfce
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-23 12:19:40 +0900

    Title: 2개씩 그룹짓기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240223/2개씩 그룹짓기/group-of-pairs.py
+++ 240223/2개씩 그룹짓기/group-of-pairs.py
@@ -1,2 +1,18 @@
 N=int(input())
 arr=list(map(int, input().split()))
+
+arr_sort=sorted(arr)
+
+sum_arr=[]
+
+while True:
+    new_arr=[arr_sort[0]] + [arr_sort[-1]]
+    arr_sort=arr_sort[1:-1]
+    sum_val=0
+    for elem in new_arr:
+        sum_val+=elem
+    sum_arr.append(sum_val)
+    if len(arr_sort)<1:
+        break
+
+print(max(sum_arr))
