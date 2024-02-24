06da4d3710702997d45d3192d2b95a9d18d014ef
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-24 23:00:09 +0900

    Title: 비오는 날, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240224/비오는 날/rainy-day.py
+++ 240224/비오는 날/rainy-day.py
@@ -13,16 +13,22 @@ class information:
 arr_info=[] # 각 n번의 입력값 넣을 list 생성
 for _ in range(n):
     date, day, weather = input().split()
-    date=date.split('-')    # date비교를 위해 
-    date[0]=int(date[0])
-    arr_info.append(information(date, day, weather))
+    date=date.split('-')    # date비교를 위해 각 값을 split해 리스트로 반환
+    for i in range(len(date)):
+        date[i]=int(date[i])    # 숫자 비교를 위해 date 값들을 int값으로 변환
+    arr_info.append(information(date, day, weather))    # 각 값들을 리스트에 추가
 
-min_val=sys.maxsize
+min_val=sys.maxsize # 최소가 되는 날짜 비교를 위해 min_val 삽입
 min_num=0
 for i in range(n):
     if arr_info[i].weather=='Rain':
         if arr_info[i].date[0]<min_val:
             min_val=arr_info[i].date[0]
             min_num=i
+
+for i in range(n):
+    for j in range(len(date)):
+        if arr_info[i].date[j]<10:
+            arr_info[i].date[j]='0' + str(arr_info[i].date[j])
 
 arr_info[min_num].say()
