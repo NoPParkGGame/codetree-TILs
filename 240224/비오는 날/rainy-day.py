aacb7920764797d303f53a9cbe9e622a71270345
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-24 23:24:46 +0900

    Title: 비오는 날, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/비오는 날/rainy-day.py
+++ 240224/비오는 날/rainy-day.py
@@ -8,37 +8,14 @@ class information:
         self.weather=weather    #class를 이용해 값 입력값 입력
 
     def say(self):
-        print(f'{"-".join(map(str, self.date))} {self.day} {self.weather}') # 메서드롤 통해 print
+        print(f'{self.date} {self.day} {self.weather}') # 메서드롤 통해 print
 
-arr_info=[] # 각 n번의 입력값 넣을 list 생성
-for _ in range(n):
-    date, day, weather = input().split()
-    date=date.split('-')    # date비교를 위해 각 값을 split해 리스트로 반환
-    for i in range(len(date)):
-        date[i]=int(date[i])    # 숫자 비교를 위해 date 값들을 int값으로 변환
-    arr_info.append(information(date, day, weather))    # 각 값들을 리스트에 추가
+arr=[tuple(input().split()) for _ in range(n)]
+info_arr=[information(date, day, weather) for date, day, weather in arr]
 
-min_val_year=sys.maxsize # 최소가 되는 날짜 비교를 위해 min_val 삽입
-min_val_month=sys.maxsize
-min_val_day=sys.maxsize
-min_num=0
-for i in range(n):
-    if arr_info[i].weather=='Rain': # Rain이 들어가는 경우에만 비교 
-        if arr_info[i].date[0]<min_val_year: # 만약 년도 값이 다르다면 바로 반환
-            min_val_year=arr_info[i].date[0]
-            min_num=i
-
-        elif arr_info[i].date==min_val_year:    #만약 년도 값이 같다면 월을 비교 해야 함
-            if arr_info[i].date[1]<min_val_month:
-                min_val_month=arr_info[i].date[1]
-                min_num=i
-            elif arr_info.date[1]==min_val_month:
-                min_val_day=arr_info[i].date[2]
-                min_num=i
+date_arr=[info_arr[i].date for i in range(n) if info_arr[i].weather=='Rain']
+date_arr=sorted(date_arr)
 
 for i in range(n):
-    for j in range(len(date)):
-        if arr_info[i].date[j]<10:
-            arr_info[i].date[j]='0' + str(arr_info[i].date[j])
-
-arr_info[min_num].say()
+    if date_arr[0]==info_arr[i].date:
+        info_arr[i].say()
