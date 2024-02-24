f4bfaa0f41e48bce2b211c8f2dd8848f881902c5
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-24 23:05:40 +0900

    Title: 비오는 날, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240224/비오는 날/rainy-day.py
+++ 240224/비오는 날/rainy-day.py
@@ -18,13 +18,23 @@ for _ in range(n):
         date[i]=int(date[i])    # 숫자 비교를 위해 date 값들을 int값으로 변환
     arr_info.append(information(date, day, weather))    # 각 값들을 리스트에 추가
 
-min_val=sys.maxsize # 최소가 되는 날짜 비교를 위해 min_val 삽입
+min_val_year=sys.maxsize # 최소가 되는 날짜 비교를 위해 min_val 삽입
+min_val_month=sys.maxsize
+min_val_day=sys.maxsize
 min_num=0
 for i in range(n):
-    if arr_info[i].weather=='Rain':
-        if arr_info[i].date[0]<min_val:
-            min_val=arr_info[i].date[0]
+    if arr_info[i].weather=='Rain': # Rain이 들어가는 경우에만 비교 
+        if arr_info[i].date[0]<min_val_year: # 만약 년도 값이 다르다면 바로 반환
+            min_val_year=arr_info[i].date[0]
             min_num=i
+
+        elif arr_info[i].date==min_val_year:    #만약 년도 값이 같다면 월을 비교 해야 함
+            if arr_info[i].date[1]<min_val_month:
+                min_val_month=arr_info[i].date[1]
+                min_num=i
+            elif arr_info.date[1]==min_val_month:
+                min_val_day=arr_info[i].date[2]
+                min_num=i
 
 for i in range(n):
     for j in range(len(date)):
