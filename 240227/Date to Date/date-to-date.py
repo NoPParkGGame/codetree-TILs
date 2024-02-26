b0e8ac6312ef8abb6628dd00e6cdb44d419d929c
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-27 01:02:41 +0900

    Title: Date to Date, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/Date to Date/date-to-date.py
+++ 240227/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ day_of_months=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 m1, d1, m2, d2= tuple(map(int, input().split()))
 
 month, day= m1, d1
-elapsed_day=0
+elapsed_day=1
 
 while True:
     if month==m2 and day==d2:
