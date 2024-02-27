ef3b2b08258dacc7fd87dae377664529736eb098
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-27 23:56:49 +0900

    Title: Date to Date, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/Date to Date/date-to-date.py
+++ 240227/Date to Date/date-to-date.py
@@ -3,20 +3,17 @@ m1, d1, m2, d2 = tuple(map(int, input().split()))
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 month, day = m1, d1
-elapsed_day=0
+elapsed_day=1
 while True:
-
-    if m1==m2 and d1==d2:
-        elapsed_day=1
 
     if month==m2 and day==d2:
         break
 
-    day+=1
     elapsed_day+=1
+    day+=1
 
     if day>num_of_days[month]:
         month+=1
-        day=0
+        day=1
 
 print(elapsed_day)
