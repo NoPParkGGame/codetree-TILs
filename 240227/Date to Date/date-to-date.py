50e670b0d1033100a83ac485a384f37c8dd5062d
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-27 23:49:37 +0900

    Title: Date to Date, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240227/Date to Date/date-to-date.py
+++ 240227/Date to Date/date-to-date.py
@@ -1,19 +1,19 @@
-day_of_months=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+m1, d1, m2, d2 = tuple(map(int, input().split()))
 
-m1, d1, m2, d2= tuple(map(int, input().split()))
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-month, day= m1, d1
+month, day = m1, d1
 elapsed_day=0
-
 while True:
+
     day+=1
-    elapsed_day+=1    
-    
+    elapsed_day+=1
+
     if month==m2 and day==d2:
         break
 
-
-    if day>day_of_months[month]:
+    if day>num_of_days[month]:
         month+=1
         day=0
+
 print(elapsed_day)
