80affa6aebb5acca2702f06433aa9be1f66419d1
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-27 23:54:45 +0900

    Title: Date to Date, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/Date to Date/date-to-date.py
+++ 240227/Date to Date/date-to-date.py
@@ -6,12 +6,11 @@ month, day = m1, d1
 elapsed_day=0
 while True:
 
-    elapsed_day+=1
-    
     if month==m2 and day==d2:
         break
 
     day+=1
+    elapsed_day+=1
 
     if day>num_of_days[month]:
         month+=1
