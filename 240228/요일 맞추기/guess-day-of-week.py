b97b1a221199d8c19ea18ea4b80692407cecd29f
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-02-28 22:29:50 +0900

    Title: 요일 맞추기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240228/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,45 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+
+day_of_month=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+day_of_week=['Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon']
+
+month, day =m1, d1
+
+elapsed_day=0
+
+while True:
+    if month==m2 and day==d2:
+        break
+    
+    day+=1
+    elapsed_day+=1
+
+    if day>day_of_month[month]:
+        month+=1
+        day=1
+
+
+print(day_of_week[elapsed_day%7])
+
+'''
++1 +8화
++2 +9수
++3 +10목
++4 +11금
++5 +12토
++6 +13일
++7 +14월
+
+즉, 7로 나누었을 때 나머지가 요일을 결정함
+
+-1 일
+-2 토
+-3 금
+-4 목
+-5 수
+-6 화
+-7 월
+
+
+'''
