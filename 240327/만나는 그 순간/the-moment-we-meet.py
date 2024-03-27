0de83f7e46dcde09a44b0569b688f6c6d654ef20
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-27 23:34:33 +0900

    Title: 만나는 그 순간, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+n, m= tuple(map(int, input().split()))
+
+move_A=[
+    input().split()
+    for _ in range(n)
+]   #A값 입력
+move_B=[
+    input().split()
+    for _ in range(m)
+]   #값 입력
+arr_A=[0]*1001
+arr_B=[0]*1001
+
+def start(move_who, arr_who):   #이동경과를 기록하는 함수 작성
+    pre_x=1
+    for drt, num in move_who:
+        num=int(num)
+        if drt=='R':
+            for i in range(pre_x, pre_x+num):
+                arr_who[i]=arr_who[i-1]+1
+        
+        if drt=='L':
+            for i in range(pre_x, pre_x+num):
+                arr_who[i]=arr_who[i-1]-1
+        pre_x=i+1
+
+start(move_A, arr_A)
+start(move_B, arr_B)    #각 함수 적용
+
+
+for i in range(1, len(arr_A)):
+    if arr_A[i]==arr_B[i]:
+        print(i)
+        break
