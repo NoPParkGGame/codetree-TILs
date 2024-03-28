0f17a5febdeaf96c7779dda212bc6667706b7d41
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-28 23:30:08 +0900

    Title: 선두를 지켜라, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240328/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,26 @@
+n,m=tuple(map(int, input().split()))
+dist_A=[0]
+dist_B=[0]
+
+def make_dist(w_num, dist_who):
+    for i in range(w_num):
+        vel, time= tuple(map(int, input().split()))
+        for i in range(time):
+            dist_who.append(vel+dist_who[-1])
+            
+make_dist(n, dist_A)
+make_dist(m, dist_B)
+
+arr_first=[
+    dist_A[i]-dist_B[i]
+    for i in range(len(dist_A))
+]
+while 0 in arr_first:
+    arr_first.remove(0)
+
+ans=0
+
+for i in range(2, len(arr_first)):
+    if arr_first[i]*arr_first[i-1]<0:
+        ans+=1
+print(ans)
