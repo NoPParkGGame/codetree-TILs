c31c81cf03229ece78c2e6adabd58fe9d7a7da96
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-29 23:22:53 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,31 @@
+num_dev, num_infect , first_infected, num_time = tuple(map(int, input().split()))
+# 개발자의수, 감염횟수, 첫번째 감염자, 몇 번의 시간이 흐르는가.
+
+arr=[
+    tuple(map(int, input().split()))       
+    for _ in range(num_time)
+]
+
+arr.sort()  # 시간 순으로 정렬
+arr_infected=[first_infected] #감염자들의 리스트를 정리
+
+for time, x_dev, y_dev in arr: #몇 초, 누가, 누구에게 unpacking
+    if num_infect==0:  # 감염가능횟수가 0이 되면 break
+        break
+    if x_dev in arr_infected:   #만약 x가 감염자 리스트에 있다면
+        arr_infected.append(y_dev)  # y 또한 감염자 리스트에 추가
+        num_infect-=1
+    elif y_dev in arr_infected: # 또는 y가 감염자 리스트에 있다면
+        arr_infected.append(y_dev)  # x 또한 감염자 리스트에 추가
+        num_infect-=1
+
+arr_dev=[
+    i
+    for i in range(1, num_dev+1)
+]
+
+for elem in arr_dev:
+    if elem in arr_infected:
+        print(1,end='')
+    else:
+        print(0,end='')
