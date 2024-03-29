bb15d71ad664cf5b9544ccc324c1c708b23d10fc
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-29 23:32:02 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -5,22 +5,24 @@ arr=[
     tuple(map(int, input().split()))       
     for _ in range(num_time)
 ]
+arr_num_infect=[0] + [num_infect]*num_dev   # 각 개발자들의 감염 횟수를 따로 관리해 주어야 함.
 
 arr.sort()  # 시간 순으로 정렬
 arr_infected=[first_infected] #감염자들의 리스트를 정리
 
 for time, x_dev, y_dev in arr: #몇 초, 누가, 누구에게 unpacking
-    if num_infect<=0:  # 감염가능횟수가 0이 되면 break
-        break
-    if x_dev in arr_infected:   #만약 x가 감염자 리스트에 있다면
+
+    if x_dev in arr_infected and arr_num_infect[x_dev]>0 :   #만약 x가 감염자 리스트에 있고 감염횟수가 0이 아니라면
+        
         arr_infected.append(y_dev)  # y 또한 감염자 리스트에 추가
-        num_infect-=1
-    elif y_dev in arr_infected: # 또는 y가 감염자 리스트에 있다면
+        arr_num_infect[x_dev]-=1
+        
+    elif y_dev in arr_infected and arr_num_infect[y_dev]>0: # 또는 y가 감염자 리스트에 있고 감염횟수가 0이 아니라면
         arr_infected.append(y_dev)  # x 또한 감염자 리스트에 추가
-        num_infect-=1
+        arr_num_infect[y_dev]-=1
+    
     elif x_dev in arr_infected and y_dev in arr_infected:
         num_infect-=1
-
 
 arr_dev=[
     i
