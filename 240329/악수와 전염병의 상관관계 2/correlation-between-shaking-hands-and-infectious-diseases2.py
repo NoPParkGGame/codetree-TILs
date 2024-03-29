b0f96423b632fa80e810f25b6c4a69ccca10255e
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-29 23:35:31 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240329/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -12,8 +12,7 @@ arr_infected=[first_infected] #감염자들의 리스트를 정리
 
 for time, x_dev, y_dev in arr: #몇 초, 누가, 누구에게 unpacking
 
-    if x_dev in arr_infected and arr_num_infect[x_dev]>0 :   #만약 x가 감염자 리스트에 있고 감염횟수가 0이 아니라면
-        
+    if x_dev in arr_infected and arr_num_infect[x_dev]>0 :   #만약 x가 감염자 리스트에 있고 감염횟수가 0이 아니라면        
         arr_infected.append(y_dev)  # y 또한 감염자 리스트에 추가
         arr_num_infect[x_dev]-=1
         
@@ -21,9 +20,10 @@ for time, x_dev, y_dev in arr: #몇 초, 누가, 누구에게 unpacking
         arr_infected.append(y_dev)  # x 또한 감염자 리스트에 추가
         arr_num_infect[y_dev]-=1
     
-    elif x_dev in arr_infected and y_dev in arr_infected:
-        num_infect-=1
-
+    elif (x_dev in arr_infected and arr_num_infect[x_dev]>0) and (y_dev in arr_infected and arr_num_infect[y_dev]>0):
+        arr_num_infect[x_dev]-=1
+        arr_num_infect[y_dev]-=1
+        
 arr_dev=[
     i
     for i in range(1, num_dev+1)
