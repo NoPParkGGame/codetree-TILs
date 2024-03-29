7a02000abff15ba0c2531c0ce88a50f9136d121e
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-03-29 22:54:06 +0900

    Title: 좌우로 움직이는 로봇, Time: 318ms, Memory: 63MB, Status: Passed - CodeTree

--- 240329/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240329/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -16,5 +16,19 @@ def distance(move_who, num):
                 move_who.append(cur)
 distance(move_A, n)
 distance(move_B, m)
-print(move_A)
-print(move_B)
+
+ans=0
+total_time=max(len(move_A), len(move_B))    # A 와 B 의 총 이동 시간이 다른 경우도 계산해줘야 함.
+
+if total_time==len(move_A): # 만약 B가 먼저 멈췄다면, 마지막 위치를 A가 추가로 움직이는 만큼 갱신해줘야함.
+    while len(move_B)<total_time:
+        move_B.append(move_B[-1])
+
+elif total_time==len(move_B):   # 그 반대의 경우도 갱신
+    while len(move_A)<total_time:
+        move_A.append(move_A[-1])
+
+for i in range(1, total_time):
+    if move_A[i]==move_B[i] and move_A[i-1] != move_B[i-1]:
+        ans+=1
+print(ans)
