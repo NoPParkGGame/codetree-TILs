f53b27136e61c6b885f14309da00c80c624834ed
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-03 22:58:47 +0900

    Title: 되돌아오기 2, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

--- 240403/되돌아오기 2/come-back-2.py
+++ 240403/되돌아오기 2/come-back-2.py
@@ -6,14 +6,13 @@ x, y=0, 0
 
 ans=-1
 for cur_time, elem in enumerate(arr, start=1):
-    if x==0 and y==0 and cur_time!=1:
-        break
     if elem=='R':
         dir_num= (dir_num+1)%4
     elif elem=='L':
         dir_num= (dir_num+3)%4
     else:
         x, y= x+dxs[dir_num], y+dys[dir_num]
-    ans=cur_time
-
+    if x==0 and y==0:    
+        ans=cur_time
+        break
 print(ans)
