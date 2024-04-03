d00c48ee8b311d51afa36d380f37c70be1e44adc
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-03 23:58:05 +0900

    Title: 거울에 레이저 쏘기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240403/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,13 +1,31 @@
 n= int(input())
-
+# 빈 리스트 생성
+arr=[
+    [0] *n
+    for _ in range(n)
+]
+# 거울 배열 정보 입력
 mirror=[
     list(input())
     for _ in range(n)
 ]
+# 레이저 쏘는 위치 입력
 laser=int(input())
+# 같은 반사각을 지닌 시작 숫자들 끼리 리스트 생성
+length=[]  
+for i in range(1, n+1):
+    length.append(i)
+for i in range(3*n-(n-1), 3*n +1):
+    length.append(i)
 
-
-
+width=[]
+for i in range(1, 4*n +1):
+    if i not in length:
+        width.append(i)
+# 숫자를 arr 내의 좌표로 치환해주는 함수 생성
+def make_axis(laser_start):
+    if laser_start in length:
+        
 
 
 '''
