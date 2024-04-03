adc99de0e554f45e52f00a4454c857ab17d89395
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-03 22:55:29 +0900

    Title: 되돌아오기 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/되돌아오기 2/come-back-2.py
@@ -0,0 +1,19 @@
+arr=list(input())
+
+dir_num=3
+dxs, dys= [1,0,-1,0], [0,-1,0,1]
+x, y=0, 0
+
+ans=-1
+for cur_time, elem in enumerate(arr):
+    if x==0 and y==0 and cur_time!=0:
+        ans=cur_time
+        break
+    if elem=='R':
+        dir_num= (dir_num+1)%4
+    elif elem=='L':
+        dir_num= (dir_num+3)%4
+    else:
+        x, y= x+dxs[dir_num], y+dys[dir_num]
+
+print(ans)
