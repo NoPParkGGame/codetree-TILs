82945f05024ebce276c040668e04777794cf07a2
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-04 23:51:25 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240404/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 240404/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -4,15 +4,18 @@ arr=[
     [0]*m
     for _ in range(n)
 ]
+
 dxs, dys= [0,1,0,-1], [1,0,-1,0]
 dir_num=0
 x,y=0, 0
+
 def in_range(x,y):
     return 0<=x and x<n and 0<=y and y<m
+    
 alph=65
 arr[x][y]=chr(alph)
-cnt=1
-while cnt<m*n:
+
+for i in range(1, m*n):
     nx, ny= x+dxs[dir_num], y+ dys[dir_num]
 
     if not in_range(nx, ny) or arr[nx][ny]!=0:
@@ -20,11 +23,10 @@ while cnt<m*n:
     
     x, y= x+dxs[dir_num], y+ dys[dir_num]
 
-    if alph>90:
+    if alph>=90:
         alph=64
     alph+=1
     arr[x][y]=chr(alph)
-    cnt+=1
 
 for row in arr:
     for elem in row:
