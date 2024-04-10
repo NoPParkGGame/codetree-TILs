a9b4dba146235620058d8471afe3c40e10098e40
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-10 23:14:52 +0900

    Title: 체크판위에서 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/체크판위에서 2/on-the-checkboard-2.py
+++ 240410/체크판위에서 2/on-the-checkboard-2.py
@@ -13,6 +13,7 @@ ans=0
 
 for i in range(1, r):
     for j in range(1, c):   # 1,1 부터 한 칸씩 완전탐색 진행
+        cur_axis= arr[0][0]
         second_detect=False # 2차 탐색은 기본 False
         if is_diff(arr[i][j]):  # 색이 다르다면 
             second_detect=True  # 2차 탐색 허가
@@ -21,7 +22,7 @@ for i in range(1, r):
         if second_detect:   # 2차 탐색 허가가 났다면
             for k in range(i+1, r-1):
                 for l in range(j+1, c-1):
-                    cur_axis=arr[k][l]
+                    cur_axis=arr[k][l]  # 이전 이동한 위치와 도착지점과 색이 다르다면
                     if is_diff(arr[r-1][c-1]) and is_diff(arr[i][j]):
                         ans+=1
 
