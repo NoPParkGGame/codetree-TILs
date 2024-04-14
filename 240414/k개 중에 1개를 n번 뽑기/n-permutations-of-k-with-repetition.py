3c77b0cf21b72dd0fe5d6f5e046e632d6c7305dd
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-14 23:43:45 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 287ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240414/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,21 @@
+k, n= tuple(map(int, input().split()))
+answer=[]
+
+def print_now():
+    for elem in answer:
+        print(elem, end=' ')
+    print()
+
+
+def make_twin(curr_num):
+    
+    if curr_num==n+n:
+        print_now()
+        return
+
+    for i in range(1, k+1):
+        answer.append(i)
+        make_twin(curr_num + 1)
+        answer.pop()        
+
+make_twin(n)
