1f5a65974ac57c99311f1a9cf87f193f57d44e61
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-15 22:44:03 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240415/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240415/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -11,16 +11,17 @@ for i in range(1, n-1):
     new_check=[]
     dist_x=0
     dist_y=0
-
     for elem in check:
         if elem!=check[i]:  # new_check = [0 0, 11 -1, 10 0]
             new_check.append(elem)
 
-    for x, y in new_check:
-        dist_x= dist_x + abs(x - dist_x); dist_y= dist_y + abs(y - dist_y)
+    for j in range(len(new_check)-1):
+        dist_x= dist_x + abs(new_check[j+1][0] - new_check[j][0])
+        dist_y= dist_y + abs(new_check[j+1][1] - new_check[j][1])
+
+
     total_dist= dist_x + dist_y
-
-
+    
     min_dist= min(min_dist, total_dist)
     
 print(min_dist)
