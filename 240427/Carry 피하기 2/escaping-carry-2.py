8e440f6e889d032b5954fca4bd73a2dd2eda62f8
NoPParkGGame <140081958+NoPParkGGame@users.noreply.github.com>
2024-04-27 22:45:37 +0900

    Title: Carry 피하기 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240427/Carry 피하기 2/escaping-carry-2.py
+++ 240427/Carry 피하기 2/escaping-carry-2.py
@@ -13,23 +13,23 @@ for i in range(n):
         arr[i]= '0' + arr[i]
 
 def is_carry(a, b, c):
-    if int(a[0]) + int(b[0]) + int(c[0])>10:
+    if int(a[0]) + int(b[0]) + int(c[0])>=10:
         return True
 
-    if int(a[1]) + int(b[1]) + int(c[1])>10:
+    if int(a[1]) + int(b[1]) + int(c[1])>=10:
         return True
 
-    if int(a[2]) + int(b[2]) + int(c[2])>10:
+    if int(a[2]) + int(b[2]) + int(c[2])>=10:
         return True
 
-    if int(a[3]) + int(b[3]) + int(c[3])>10:
+    if int(a[3]) + int(b[3]) + int(c[3])>=10:
         return True
 
     return False
 
 def sum_all(a, b, c):
-    a=int(a); b=int(b); c=int(c)
-    return a+b+c
+
+    return int(a)+int(b)+int(c)
 
 for i in range(n):
     for j in range(i+1, n):
@@ -37,7 +37,6 @@ for i in range(n):
 
             if not is_carry(arr[i],arr[j],arr[k]):
                 total_val = sum_all(arr[i], arr[j], arr[k])
-
                 max_val= max(max_val, total_val)
                 ans=max_val
 
